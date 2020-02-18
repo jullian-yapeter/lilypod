62665689006d7763b2995db345ead92757c480bc
jullian-yapeter <jullianyapeter@gmail.com>
2020-02-18 18:46:15 -0500

    last commit of the day

--- Rpi/serialcomm/serialcomm.py
+++ Rpi/serialcomm/serialcomm.py
@@ -6,8 +6,6 @@ import socket
 import struct
 import time
 
-import binascii
-
 
 class CommManager():
     def __init__(self, host=socket.gethostname(), port=8081):
@@ -25,13 +23,13 @@ class Serialcomm:
     def __init__(self, baudrate, slaveport='/dev/ttyACM0'):
         self.commManager = CommManager(host=socket.gethostname(), port=8082)
         self.messageLength = 10
-        # self.ser = serial.Serial(
-        #     port=slaveport,
-        #     baudrate=baudrate,
-        #     parity=serial.PARITY_NONE,
-        #     stopbits=serial.STOPBITS_ONE,
-        #     bytesize=serial.EIGHTBITS,
-        #     timeout=1)
+        self.ser = serial.Serial(
+            port=slaveport,
+            baudrate=baudrate,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=1)
 
     # Function to concatenate message that is received in pieces
     def recvall(self, sock, n):
